4b19319ac6f288f57d2355775daf2fafbc82a0be
kimwj9792 <kuj0902@gmail.com>
2021-11-01 11:05:09 +0900

    chore: 프로젝트 생성

---
+++ OwnMyWay/OwnMyWay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OwnMyWay
+//
+//  Created by 김우재 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
