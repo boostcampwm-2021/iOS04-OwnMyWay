c99edf3a028383829f234c3d84de0461c7b4785e
mandeuk26 <beddl0626@gmail.com>
2021-11-02 18:34:16 +0900

    feat: LandmarkCart Usecase, ViewModel 생성

---
+++ OwnMyWay/OwnMyWay/Usecase/LandmarkCartUsecase.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkCartUsecase.swift
+//  OwnMyWay
+//
+//  Created by 강현준 on 2021/11/02.
+//
+
+import Foundation
+
+enum LandmarkCartError: Error {
+
+}
+
+protocol LandmarkCartUsecase {
+
+}
+
+class DefaultLandmarkCartUsecase: LandmarkCartUsecase {
+
+}
+
---
+++ OwnMyWay/OwnMyWay/ViewModel/LandmarkCartViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkCartViewModel.swift
+//  OwnMyWay
+//
+//  Created by 강현준 on 2021/11/02.
+//
+
+import Combine
+import Foundation
+
+protocol LandmarkCartViewModelType {
+
+}
+
+class LandmarkCartViewModel: LandmarkCartViewModelType, ObservableObject {
+
+}
