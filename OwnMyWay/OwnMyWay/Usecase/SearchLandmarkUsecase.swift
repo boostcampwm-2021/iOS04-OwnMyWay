191ce859030ef628d91b801383386bcbb8dad61d
kimwj9792 <kuj0902@gmail.com>
2021-11-03 01:43:27 +0900

    feat: SearchLandmarkUsecase Search 기능 추가

--- OwnMyWay/OwnMyWay/Usecase/SearchLandmarkUsecase.swift
+++ OwnMyWay/OwnMyWay/Usecase/SearchLandmarkUsecase.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol SearchLandmarkUsecase {
     func executeFetch(completion: @escaping ([LandmarkDTO]) -> Void)
+    func executeSearch(searchText: String, completion: @escaping ([LandmarkDTO]) -> Void)
 }
 
 class DefaultSearchLandmarkUsecase: SearchLandmarkUsecase {
@@ -24,6 +25,18 @@ class DefaultSearchLandmarkUsecase: SearchLandmarkUsecase {
             switch result {
             case .success(let landmarkDTOs):
                 completion(landmarkDTOs)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func executeSearch(searchText: String, completion: @escaping ([LandmarkDTO]) -> Void) {
+        landmarkDTORepository.fetchLandmarkDTOs { result in
+            switch result {
+            case .success(let landmarkDTOs):
+                let searchResult = landmarkDTOs.filter { $0.title.contains(searchText) }
+                completion(searchResult)
             case .failure(let error):
                 print(error)
             }
