6e853dbbc7a0e538d2718d90e72c0e09c249e9d0
kimwj9792 <kuj0902@gmail.com>
2021-11-02 14:55:41 +0900

    refactor: CoreData Repository 리뷰 반영

--- OwnMyWay/OwnMyWay/Repository/TravelRepository.swift
+++ OwnMyWay/OwnMyWay/Repository/TravelRepository.swift
@@ -5,12 +5,13 @@
 //  Created by 김우재 on 2021/11/02.
 //
 
-import Foundation
 import CoreData
 import UIKit
 
 protocol TravelRepository {
-
+    func addTravel(title: String, startDate: Date, endDate: Date) -> Result<Travel, Error>
+    func fetchAll() -> Result<[Travel], Error>
+    func findTravelById(objectID: NSManagedObjectID) -> Travel?
 }
 
 class CoreDataTravelRepository: TravelRepository {
@@ -24,20 +25,29 @@ class CoreDataTravelRepository: TravelRepository {
         return newContext
     }()
 
-    func addTravel(title: String, startDate: Date, endDate: Date) throws {
+    func addTravel(title: String, startDate: Date, endDate: Date) -> Result<Travel, Error> {
         guard let entity = NSEntityDescription.entity(forEntityName: "Travel", in: context) else {
-            return
+            return .failure(NSError.init())
         }
         let travel = Travel(entity: entity, insertInto: context)
         travel.setValue(title, forKey: "title")
         travel.setValue(startDate, forKey: "startDate")
         travel.setValue(endDate, forKey: "endDate")
-        try context.save()
+        do {
+            try context.save()
+            return .success(travel)
+        } catch let error {
+            return .failure(error)
+        }
     }
 
-    func fetchAll() throws -> [Travel] {
-        let travels = try context.fetch(Travel.fetchRequest())
-        return travels
+    func fetchAll() -> Result<[Travel], Error> {
+        do {
+            let travels = try context.fetch(Travel.fetchRequest())
+            return .success(travels)
+        } catch let error {
+            return .failure(error)
+        }
     }
 
     func findTravelById(objectID: NSManagedObjectID) -> Travel? {
