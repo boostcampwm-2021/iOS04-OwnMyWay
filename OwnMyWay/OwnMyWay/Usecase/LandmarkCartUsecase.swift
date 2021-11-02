932e49679c979e2a0589f3279a90526b8d884af4
mandeuk26 <beddl0626@gmail.com>
2021-11-02 19:06:28 +0900

    feat: LandmarkCartUsecase 구현

--- OwnMyWay/OwnMyWay/Usecase/LandmarkCartUsecase.swift
+++ OwnMyWay/OwnMyWay/Usecase/LandmarkCartUsecase.swift
@@ -8,14 +8,44 @@
 import Foundation
 
 enum LandmarkCartError: Error {
-
+    case registerFail
 }
 
 protocol LandmarkCartUsecase {
-
+    func addLandmark(to travel: Travel, of landmark: Landmark, completion: (Error) -> Void)
+    func delete(of travel: Travel)
+    func delete(of landmark: Landmark)
 }
 
 class DefaultLandmarkCartUsecase: LandmarkCartUsecase {
+    let travelRepository: TravelRepository
 
+    init(travelRepository: TravelRepository) {
+        self.travelRepository = travelRepository
+    }
+
+    func addLandmark(to travel: Travel, of landmark: Landmark, completion: (Error) -> Void) {
+        guard let title = landmark.title, let image = landmark.image else { return }
+        let latitude = landmark.latitude, longitude = landmark.longitude
+
+        let result = self.travelRepository.addLandmark(to: travel,
+                                                       title: title,
+                                                       image: image,
+                                                       latitude: latitude,
+                                                       longitude: longitude)
+        switch result {
+        case .success: break
+        case .failure: completion(LandmarkCartError.registerFail)
+        }
+    }
+
+    func delete(of travel: Travel) {
+        // self.travelRepository.deleteTravel()
+        // 구현해주세요 우재님 ^_^
+    }
+
+    func delete(of landmark: Landmark) {
+//        self.travelRepository.deleteLandmark()
+//        구현해주세요 우재님 ^_^
+    }
 }
-
