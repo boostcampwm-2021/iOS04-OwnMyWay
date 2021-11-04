8ddb92f88bd97f8b4906b8fa03991a703e950855
mandeuk26 <beddl0626@gmail.com>
2021-11-04 18:41:24 +0900

    feat: 새로운 여행 화면, 랜드마크 카트화면 전환 구현

--- OwnMyWay/OwnMyWay/ViewController/AddLandmarkViewController.swift
+++ OwnMyWay/OwnMyWay/ViewController/AddLandmarkViewController.swift
@@ -9,16 +9,14 @@ import UIKit
 
 class AddLandmarkViewController: UIViewController, Instantiable {
     @IBOutlet weak var cartView: UIView!
+    var closure: ((UIView) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let cartVC = LandmarkCartViewController.instantiate(storyboardName: "LandmarkCart")
-        addChild(cartVC)
-        self.cartView.addSubview(cartVC.view)
-        cartVC.view.translatesAutoresizingMaskIntoConstraints = false
-        cartVC.view.topAnchor.constraint(equalTo: cartView.topAnchor).isActive = true
-        cartVC.view.leadingAnchor.constraint(equalTo: cartView.leadingAnchor).isActive = true
-        cartVC.view.trailingAnchor.constraint(equalTo: cartView.trailingAnchor).isActive = true
-        cartVC.view.bottomAnchor.constraint(equalTo: cartView.bottomAnchor).isActive = true
+        self.closure?(self.cartView)
+    }
+
+    func bind(closure: @escaping (UIView) -> Void) {
+        self.closure = closure
     }
 }
