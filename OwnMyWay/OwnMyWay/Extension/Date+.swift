4246dfe4bd77430bc072acc1eb35975945ab7ff9
kimwj9792 <kuj0902@gmail.com>
2021-11-10 18:12:36 +0900

    feat: Date 한글 extension 추가

--- OwnMyWay/OwnMyWay/Extension/Date+.swift
+++ OwnMyWay/OwnMyWay/Extension/Date+.swift
@@ -16,6 +16,14 @@ extension Date {
         return dateFormatter.string(from: self)
     }
 
+    func toKorean() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        return dateFormatter.string(from: self)
+    }
+    
     func format(endDate: Date?) -> String {
         guard let endDate = endDate,
               self <= endDate else {
