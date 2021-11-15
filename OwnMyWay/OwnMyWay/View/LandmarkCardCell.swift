b8a7e061284a89a26065fda7a357edaf71b5761b
HanjunYu <hj56775@gmail.com>
2021-11-15 12:57:56 +0900

    feat: LandmarkCell Shake 함수 구현

--- OwnMyWay/OwnMyWay/View/LandmarkCardCell.swift
+++ OwnMyWay/OwnMyWay/View/LandmarkCardCell.swift
@@ -31,4 +31,27 @@ class LandmarkCardCell: UICollectionViewCell {
         self.imageView.setImage(with: landmark.image)
         self.titleLabel.text = landmark.title
     }
+
+    func shake() {
+        let shakeAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        shakeAnimation.duration = 0.05
+        shakeAnimation.repeatCount = 2
+        shakeAnimation.autoreverses = true
+        let startAngle: Float =  -3.14159 / 180
+        let stopAngle = -startAngle
+        shakeAnimation.fromValue = NSNumber(value: startAngle as Float)
+        shakeAnimation.toValue = NSNumber(value: 2 * stopAngle as Float)
+        shakeAnimation.autoreverses = true
+        shakeAnimation.duration = 0.15
+        shakeAnimation.repeatCount = 10000
+        shakeAnimation.timeOffset = 290 * Double.random(in: 0...1)
+
+        let layer: CALayer = self.layer
+        layer.add(shakeAnimation, forKey: "shaking")
+    }
+
+    func stopShaking() {
+        let layer: CALayer = self.layer
+        layer.removeAnimation(forKey: "shaking")
+    }
 }
