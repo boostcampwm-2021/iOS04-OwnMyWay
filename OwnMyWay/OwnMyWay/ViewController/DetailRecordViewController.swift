e28c5e0942a45a6fa839e909dd21df38e4479c0a
HanjunYu <hj56775@gmail.com>
2021-11-16 13:55:39 +0900

    feat: image Paging 기능 구현

--- OwnMyWay/OwnMyWay/ViewController/DetailRecordViewController.swift
+++ OwnMyWay/OwnMyWay/ViewController/DetailRecordViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class DetailRecordViewController: UIViewController, Instantiable {
 
+    @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var imageStackView: UIStackView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var titleLabel: UILabel!
@@ -25,5 +26,23 @@ class DetailRecordViewController: UIViewController, Instantiable {
 
     func bind(viewModel: DetailRecordViewModel) {
         self.viewModel = viewModel
+    }
+    
+    // FIXME: ViewModel record에 바인딩?
+    private func configurePageControl() {
+        guard let numberOfphotos = self.viewModel?.record.photoURLs?.count else { return }
+        self.pageControl.numberOfPages = numberOfphotos
+    }
+    
+    private func configurePageControlSelectedPage(currentPage: Int) {
+        self.pageControl.currentPage = currentPage
+    }
+}
+
+// MARK: - UIScollViewDelegate
+extension DetailRecordViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let currentPage = Int(round(self.imageScrollView.contentOffset.x / self.imageScrollView.frame.size.width))
+        self.configurePageControlSelectedPage(currentPage: currentPage)
     }
 }
