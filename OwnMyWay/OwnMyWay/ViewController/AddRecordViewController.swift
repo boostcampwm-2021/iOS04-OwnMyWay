e78eb7360f8a813782cf96349ff438c4f3f32eb1
청수 <bestowing@naver.com>
2021-11-16 16:20:30 +0900

    feat: AddRecordVC 네비게이션바 백버튼 삭제

--- OwnMyWay/OwnMyWay/ViewController/AddRecordViewController.swift
+++ OwnMyWay/OwnMyWay/ViewController/AddRecordViewController.swift
@@ -55,13 +55,6 @@ class AddRecordViewController: UIViewController, Instantiable {
 
     private func configureNavigation() {
         self.navigationItem.title = "게시물 작성"
-
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
-            image: UIImage(systemName: "chevron.backward"),
-            style: .plain,
-            target: self,
-            action: #selector(backButtonAction)
-        )
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(
             title: "완료",
             style: .plain,
