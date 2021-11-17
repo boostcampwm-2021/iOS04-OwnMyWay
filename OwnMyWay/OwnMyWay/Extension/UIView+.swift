605dd61e7fa66595c7ed2c94065c35c692bde07e
HanjunYu <hj56775@gmail.com>
2021-11-17 10:47:25 +0900

    feat: makePolaroid 함수 구현

---
+++ OwnMyWay/OwnMyWay/Extension/UIView+.swift
@@ -0,0 +1,64 @@
+//
+//  UIView+.swift
+//  OwnMyWay
+//
+//  Created by 유한준 on 2021/11/17.
+//
+
+import UIKit
+
+extension UIView {
+    func makePolaroid(with record: Record) {
+        let photoImageView = UIImageView()
+        photoImageView.setImage(with: record.photoURLs?.first)
+        let titleLabel = UILabel()
+        titleLabel.text = record.content
+        titleLabel.font = UIFont(name: "Chalkduster", size: 17)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(photoImageView)
+        self.addSubview(titleLabel)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.systemGray6.cgColor
+        self.layer.shadowOpacity = 1
+        self.layer.shadowColor = UIColor.systemGray5.cgColor
+        self.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.layer.shadowRadius = 3
+        self.layer.masksToBounds = false
+        NSLayoutConstraint.activate([
+            photoImageView.topAnchor.constraint(
+                equalTo: self.topAnchor,
+                constant: 20
+            ),
+            photoImageView.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor,
+                constant: 20
+            ),
+            photoImageView.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor,
+                constant: -20
+            ),
+            photoImageView.heightAnchor.constraint(
+                equalTo: self.widthAnchor,
+                multiplier: 0.75
+            ),
+            titleLabel.topAnchor.constraint(
+                equalTo: photoImageView.bottomAnchor,
+                constant: 15
+            ),
+            titleLabel.bottomAnchor.constraint(
+                equalTo: self.bottomAnchor,
+                constant: -15
+            ),
+            titleLabel.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor,
+                constant: 24
+            ),
+            titleLabel.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor,
+                constant: -24
+            )
+        ])
+    }
+}
