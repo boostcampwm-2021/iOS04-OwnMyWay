df923a717665bf21dcd5829680298eea2d44168a
청수 <bestowing@naver.com>
2021-11-22 19:11:54 +0900

    feat: HomeBackgroundView 추가
    UICollectionReusableView, 홈 콜렉션뷰의 배경화면임

---
+++ OwnMyWay/OwnMyWay/View/HomeBackgroundView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeBackgroundView.swift
+//  OwnMyWay
+//
+//  Created by 이청수 on 2021/11/22.
+//
+
+import UIKit
+
+final class HomeBackgroundView: UICollectionReusableView {
+    @IBOutlet private weak var backgroundView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+
+    private func configure() {
+        self.layer.cornerRadius = 25
+    }
+
+}
