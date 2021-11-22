d0f6f6b5640c746b1af7d42c0de56f82cecbc0b3
청수 <bestowing@naver.com>
2021-11-22 17:20:43 +0900

    design: 홈화면 font 설정

--- OwnMyWay/OwnMyWay/View/TravelSectionHeader.swift
+++ OwnMyWay/OwnMyWay/View/TravelSectionHeader.swift
@@ -11,8 +11,12 @@ class TravelSectionHeader: UICollectionReusableView {
     static let identifier = "TravelSectionHeader"
 
     @IBOutlet private weak var sectionTitleLabel: UILabel!
+    private let fontName = "Apple SD 산돌고딕 Neo"
+    private let fontSize: CGFloat = 25
 
     func configure(sectionTitle: String) {
+        let font = UIFont(name: self.fontName, size: self.fontSize)
+        self.sectionTitleLabel.font = font ?? UIFont.systemFont(ofSize: self.fontSize)
         self.sectionTitleLabel.text = sectionTitle
     }
 
