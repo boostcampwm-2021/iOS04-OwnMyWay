f48b2ea52e331bf868a47c96ad964423e602787c
청수 <bestowing@naver.com>
2021-11-23 15:58:54 +0900

    feat: MessageCell 추가

---
+++ OwnMyWay/OwnMyWay/View/MessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  MessageCell.swift
+//  OwnMyWay
+//
+//  Created by 이청수 on 2021/11/23.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    static let identifier = "MessageCell"
+
+}
