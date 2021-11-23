46f37a171e95b53d1ce2193c66266007c1cb9b4e
청수 <bestowing@naver.com>
2021-11-23 16:37:55 +0900

    feat: messageCell 터치 이벤트 전달 delegate 생성

--- OwnMyWay/OwnMyWay/View/MessageCell.swift
+++ OwnMyWay/OwnMyWay/View/MessageCell.swift
@@ -7,7 +7,23 @@
 
 import UIKit
 
+protocol MessageCellDelegate: AnyObject {
+    func didTouchButton()
+}
+
 class MessageCell: UICollectionViewCell {
     static let identifier = "MessageCell"
+
+    @IBOutlet private weak var createTravelButton: UIButton!
+    weak var delegate: MessageCellDelegate?
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.createTravelButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func didTouchCreateTravelButton(_ sender: UIButton) {
+        self.delegate?.didTouchButton()
+    }
 
 }
