5a9b071f04c51e2d09c870d32826fb2828177fad
청수 <bestowing@naver.com>
2021-11-25 21:00:01 +0900

    design: 캘린더 디자인 요구사항 반영

--- OwnMyWay/OwnMyWay/View/CalendarCell.swift
+++ OwnMyWay/OwnMyWay/View/CalendarCell.swift
@@ -14,15 +14,44 @@ class CalendarCell: UICollectionViewCell {
     @IBOutlet private weak var dateLabel: UILabel!
     @IBOutlet private weak var backgroundCellView: UIView!
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundCellView.layer.cornerRadius = self.backgroundCellView.frame.size.height / 2
+    }
+
     func configure(item: CalendarDataSource.CalendarItem) {
         self.dateLabel.text = item.isDummy ? "" : "\(item.date.dayNumber)"
-        self.backgroundCellView.backgroundColor = .clear
-        self.dateLabel.textColor = .label
+        let weekday = item.date.weekday
+        switch weekday {
+        case 1:
+            self.dateLabel.textColor = .red
+        case 7:
+            self.dateLabel.textColor = .blue
+        default:
+            self.dateLabel.textColor = .label
+        }
+        if item.date.isToday() && !item.isDummy {
+            self.backgroundCellView.backgroundColor = UIColor(named: "WJGreen")
+            self.dateLabel.textColor = .white
+        } else {
+            self.backgroundCellView.backgroundColor = .clear
+        }
     }
 
     func didSelect() {
         self.backgroundCellView.backgroundColor = UIColor(named: "IdentityBlue") ?? .blue
         self.dateLabel.textColor = .white
+    }
+
+}
+
+fileprivate extension Date {
+
+    func isToday() -> Bool {
+        let today = Date()
+        return today.year == self.year &&
+        today.month == self.month &&
+        today.dayNumber == self.dayNumber
     }
 
 }
