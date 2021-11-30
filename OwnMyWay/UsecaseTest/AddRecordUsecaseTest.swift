a50a32d1842d1c618d5085c27f195c17bbd2e066
mandeuk26 <beddl0626@gmail.com>
2021-11-30 23:12:58 +0900

    test: AddRecordUsecase 테스트 코드 추가

---
+++ OwnMyWay/UsecaseTest/AddRecordUsecaseTest.swift
@@ -0,0 +1,57 @@
+//
+//  UsecaseTest.swift
+//  UsecaseTest
+//
+//  Created by 강현준 on 2021/11/30.
+//
+
+import XCTest
+import CoreData
+
+class AddRecordUsecaseTest: XCTestCase {
+    class MokContextFetcher: ContextAccessable {
+        func fetchContext() -> NSManagedObjectContext {
+            return NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        }
+    }
+    
+    var usecase: AddRecordUsecase!
+    
+    override func setUpWithError() throws {
+        self.usecase = DefaultAddRecordUsecase(
+            repository: CoreDataTravelRepository(contextFetcher: MokContextFetcher()),
+            imageFileManager: ImageFileManager.shared
+        )
+    }
+
+    override func tearDownWithError() throws {
+        self.usecase = nil
+    }
+    
+    func test_타이틀_검증() {
+        let testTitle1 = "Test입니다."
+        let testTitle2 = ""
+        let testTitle3 = "Test입니다.Test입니다.Test입니다.Test입니다."
+        XCTAssertTrue(self.usecase.executeValidationTitle(with: testTitle1))
+        XCTAssertFalse(self.usecase.executeValidationTitle(with: testTitle2))
+        XCTAssertFalse(self.usecase.executeValidationTitle(with: testTitle3))
+    }
+    
+    func test_날짜_검증() {
+        let date = Date()
+        let nilDate: Date? = nil
+        XCTAssertTrue(self.usecase.executeValidationDate(with: date))
+        XCTAssertFalse(self.usecase.executeValidationDate(with: nilDate))
+    }
+    
+    func test_위치_검증() {
+        let location1 = Location(latitude: -90, longitude: -180)
+        let location2 = Location(latitude: 90, longitude: 180)
+        let location3 = Location(latitude: -91, longitude: -180)
+        let location4 = Location(latitude: 90, longitude: 181)
+        XCTAssertTrue(self.usecase.executeValidationCoordinate(with: location1))
+        XCTAssertTrue(self.usecase.executeValidationCoordinate(with: location2))
+        XCTAssertFalse(self.usecase.executeValidationCoordinate(with: location3))
+        XCTAssertFalse(self.usecase.executeValidationCoordinate(with: location4))
+    }
+}
