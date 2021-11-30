c482ab7e898abdab5535a3b112d0bf08883de74f
mandeuk26 <beddl0626@gmail.com>
2021-11-30 18:33:06 +0900

    test: EnterTitleViewModel 테스트 코드 작성

--- OwnMyWay/ViewModelTest/EnterTitleViewModelTest.swift
+++ OwnMyWay/ViewModelTest/EnterTitleViewModelTest.swift
@@ -5,28 +5,76 @@
 //  Created by 김우재 on 2021/11/30.
 //
 
+import Combine
 import XCTest
 
 class EnterTitleViewModelTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var viewModel: DefaultEnterTitleViewModel!
+    var coordinator: MockCoordinator!
+    
+    class MockCoordinator: EnterTitleCoordinatingDelegate {
+        var travel: Travel?
+        
+        func pushToEnterDate(travel: Travel, isEditingMode: Bool) {
+            self.travel = travel
+            return
         }
+    }
+    
+    override func setUp() {
+        coordinator = MockCoordinator()
+        viewModel = DefaultEnterTitleViewModel(
+            usecase: DefaultEnterTitleUsecase(),
+            coordinatingDelegate: self.coordinator,
+            travel: nil
+        )
+    }
+
+    override func tearDown() {
+        coordinator = nil
+        viewModel = nil
+    }
+    
+    func test_텍스트_입력() {
+        let expectation = XCTestExpectation()
+
+        var cancellable: AnyCancellable = self.viewModel.validatePublisher.sink { result in
+            if result == nil { return }
+            
+            if result == true {
+                expectation.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        
+        self.viewModel.didChangeTitle(text: "테스트입니다.")
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func test_텍스트_입력_실패() {
+        let expectation = XCTestExpectation()
+
+        var cancellable: AnyCancellable = self.viewModel.validatePublisher.sink { result in
+            if result == nil { return }
+            
+            if result == true {
+                XCTFail()
+            } else {
+                expectation.fulfill()
+            }
+        }
+        
+        self.viewModel.didChangeTitle(text: "")
+        wait(for: [expectation], timeout: 1)
+    }
+
+    func test_다음_버튼_클릭() {
+        self.viewModel.didChangeTitle(text: "테스트입니다.")
+        self.viewModel.didTouchNextButton()
+        XCTAssertNotNil(self.coordinator.travel)
+        XCTAssertTrue(self.coordinator.travel?.title == "테스트입니다.")
     }
 
 }
