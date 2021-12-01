1194635f7ab0dca219255796749c6903b5a54df8
청수 <bestowing@naver.com>
2021-12-01 16:06:57 +0900

    test: 내용입력 테스트 함수까지 구현함

--- OwnMyWay/ViewModelTest/AddRecordViewModelTest.swift
+++ OwnMyWay/ViewModelTest/AddRecordViewModelTest.swift
@@ -9,26 +9,143 @@ import XCTest
 
 class AddRecordViewModelTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    enum TestError: Error {
+    case error
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    var creatingViewModel: AddRecordViewModel!
+    var editingViewModel: AddRecordViewModel!
+    private let testDate = Date(timeIntervalSince1970: 0)
+
+    class MockCoordinator: AddRecordCoordinatingDelegate {
+        func popToParent(with record: Record) {}
+        
+        func presentToSearchLocation() {}
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    class MockUsecase: AddRecordUsecase {
+        private let isValidateUsecase: Bool
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        init(isValidate: Bool) {
+            isValidateUsecase = isValidate
+        }
+
+        func executeValidationTitle(with title: String?) -> Bool {
+            return self.isValidateUsecase
+        }
+        
+        func executeValidationDate(with date: Date?) -> Bool {
+            return self.isValidateUsecase
+        }
+        
+        func executeValidationCoordinate(with coordinate: Location) -> Bool {
+            return self.isValidateUsecase
+        }
+        
+        func executePickingPhoto(with url: URL, completion: (Result<String, Error>) -> Void) {
+            if self.isValidateUsecase {
+                completion(.success("성공"))
+            } else {
+                let error = TestError.error
+                completion(.failure(error))
+            }
+        }
+        
+        func executeRemovingPhoto(of photoID: String, completion: (Result<Void, Error>) -> Void) {
+            if self.isValidateUsecase {
+                completion(.success(Void()))
+            } else {
+                let error = TestError.error
+                completion(.failure(error))
+            }
         }
     }
 
-}
+    override func setUp() {
+        super.setUp()
+        self.creatingViewModel = DefaultAddRecordViewModel(
+            record: nil, usecase: MockUsecase(isValidate: true),
+            coordinatingDelegate: MockCoordinator(), isEditingMode: false
+        )
 
-// 청수
+        let record = Record(uuid: UUID(), title: "테스트제목", content: "테스트내용", date: testDate, latitude: 0, longitude: 0, photoIDs: [], placeDescription: "테스트장소설명")
+        self.editingViewModel = DefaultAddRecordViewModel(
+            record: record, usecase: MockUsecase(isValidate: true),
+            coordinatingDelegate: MockCoordinator(), isEditingMode: true
+        )
+    }
+
+    override func tearDown() {
+        self.creatingViewModel = nil
+        self.editingViewModel = nil
+        super.tearDown()
+    }
+
+    func test_장소입력() {
+        let testRecordPlace = "테스트제목"
+        let testLatitude: Double = 1
+        let testLongtitude: Double = 1
+        self.creatingViewModel.locationDidUpdate(
+            recordPlace: testRecordPlace, latitude: testLatitude, longitude: testLongtitude
+        )
+        XCTAssertEqual(self.creatingViewModel.record.placeDescription, testRecordPlace)
+        XCTAssertEqual(self.creatingViewModel.record.latitude, testLatitude)
+        XCTAssertEqual(self.creatingViewModel.record.longitude, testLongtitude)
+    }
+
+    func test_제목입력() {
+        let testTitle = "테스트타이틀"
+        self.creatingViewModel.didEnterTitle(with: testTitle)
+        XCTAssertEqual(self.creatingViewModel.record.title, testTitle)
+    }
+
+    func test_시간입력() {
+        let interval: Double = 100
+        let testDate = Date(timeIntervalSince1970: interval)
+        self.creatingViewModel.didEnterTime(with: testDate)
+        XCTAssertEqual(self.creatingViewModel.record.date, testDate)
+    }
+
+    func test_좌표입력() {
+        let testLatitude: Double = 1
+        let testLongtitude: Double = 1
+        self.creatingViewModel.didEnterCoordinate(latitude: testLatitude, longitude: testLongtitude)
+        XCTAssertEqual(self.creatingViewModel.record.latitude, testLatitude)
+        XCTAssertEqual(self.creatingViewModel.record.longitude, testLongtitude)
+    }
+
+    func test_내용입력() {
+        let testContent = "테스트내용"
+        self.creatingViewModel.didEnterContent(with: testContent)
+        XCTAssertEqual(self.creatingViewModel.record.content, testContent)
+    }
+
+    func test_사진_URL_입력() {
+        
+    }
+
+    func test_사진제거() {
+        
+    }
+
+    func test_제출버튼_터치() {
+        
+    }
+
+    func test_장소버튼_터치() {
+        
+    }
+
+    func test_뒤로버튼_터치() {
+        
+    }
+
+    func test_좌표변경() {
+        
+    }
+
+    func test_장소이름() {
+        
+    }
+
+}
